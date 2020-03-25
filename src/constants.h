10b5b110cc0244bff22f7f3dfdfb0af633ffa2cd
Edward Fang <edward.fang@berkeley.edu>
2020-03-25 13:37:22 -0700

    updating constants

--- src/constants.h
+++ src/constants.h
@@ -1,11 +1,12 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 const float BOT_CLEARANCE = 0.1;
-const float STEP_SIZE = 3.0;
+const float STEP_SIZE = 2.5;
 const float COMPLETION_THRESHOLD = 1.0;
-const float RADIUS = 6.0;
-const float RAD_STEP = 1.0;
-const float RAD_RANGE = 1.0;
+const float RADIUS = 7.5;
+const float RAD_STEP = 2.5;
+const float RAD_UPPER_RANGE = 2.5;
+const float RAD_LOWER_RANGE = 0;
 const float DEFAULT_CAR_LENGTH = 4.8;
 const float DEFAULT_CAR_WIDTH = 1.8;
 #endif // CONSTANTS_H
