c5124222d8e944338eddbf533cf0c0a9be25c931
Ionel Gog <gogionel@gmail.com>
2020-06-08 20:01:14 -0700

    Import library using PYLOT_HOME environment variable.

--- HybridAStar/hybrid_astar_wrapper.py
+++ HybridAStar/hybrid_astar_wrapper.py
@@ -17,20 +17,17 @@ except:
 try:
     cdll = CDLL("build/libHybridAStar.so")
 except:
-    cdll = CDLL(
-        "{}/pylot/planning/hybrid_astar/hybrid_astar_planner/"
-        "build/libHybridAStar.so".format(os.getcwd())
-    )
+    cdll = CDLL("{}/pylot/planning/hybrid_astar/hybrid_astar_planner/"
+                "build/libHybridAStar.so".format(os.getenv("PYLOT_HOME")))
 _c_double_p = POINTER(c_double)
 
 # func / return type declarations for C++ ApplyHybridAStar
 _apply_hybrid_astar = cdll.ApplyHybridAStar
-_apply_hybrid_astar.argtypes = (
-    POINTER(HybridAStarInitialConditions),
-    POINTER(HybridAStarHyperparameters),
-    POINTER(HybridAStarReturnValues)
-)
+_apply_hybrid_astar.argtypes = (POINTER(HybridAStarInitialConditions),
+                                POINTER(HybridAStarHyperparameters),
+                                POINTER(HybridAStarReturnValues))
 _apply_hybrid_astar.restype = None
+
 
 def apply_hybrid_astar(initial_conditions, hyperparameters):
     """ Run HybridAStar given initial conditions and hyperparameters.
@@ -74,13 +71,15 @@ def apply_hybrid_astar(initial_conditions, hyperparameters):
     success = hastar_rv.success
     x_path = np.array([hastar_rv.x_path[i] for i in range(MAX_PATH_LENGTH)])
     y_path = np.array([hastar_rv.y_path[i] for i in range(MAX_PATH_LENGTH)])
-    yaw_path = np.array([hastar_rv.yaw_path[i] for i in range(MAX_PATH_LENGTH)])
+    yaw_path = np.array(
+        [hastar_rv.yaw_path[i] for i in range(MAX_PATH_LENGTH)])
 
     ind = -1
     if success and np.any(np.isnan(x_path)):
         ind = np.where(np.isnan(x_path))[0][0]
 
     return x_path[:ind], y_path[:ind], yaw_path[:ind], success
+
 
 def to_hastar_initial_conditions(initial_conditions):
     x_start = initial_conditions['start'][0]
@@ -101,12 +100,12 @@ def to_hastar_initial_conditions(initial_conditions):
         x_end,
         y_end,
         yaw_end,
-        o_llx.ctypes.data_as(_c_double_p), # obstacles lower left x
-        o_lly.ctypes.data_as(_c_double_p), # obstacles lower left y
-        o_urx.ctypes.data_as(_c_double_p), # obstacles upper right x
-        o_ury.ctypes.data_as(_c_double_p), # obstacles upper right y
-        len(obs)
-    )
+        o_llx.ctypes.data_as(_c_double_p),  # obstacles lower left x
+        o_lly.ctypes.data_as(_c_double_p),  # obstacles lower left y
+        o_urx.ctypes.data_as(_c_double_p),  # obstacles upper right x
+        o_ury.ctypes.data_as(_c_double_p),  # obstacles upper right y
+        len(obs))
+
 
 def _parse_hyperparameters(hp):
     return HybridAStarHyperparameters(
